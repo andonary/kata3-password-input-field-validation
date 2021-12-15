4e8b376647163352746879f7c17e127c000dbc2f
Ando Nary <ando.nary@gmail.com>
2021-12-15 08:45:20 +0100

    partie 4

--- src/passwordValidation.ts
+++ src/passwordValidation.ts
@@ -14,7 +14,7 @@ function isLongEnough(password: string) {
     return password.length > 7;
 }
 
-function isContainEnoughNumbers(password: string) {
+function hasEnoughNumbers(password: string) {
     const numberOfNumbers: number = password.split("").reduce((acc, val) => {
         if (isNaN(Number(val))) return acc;
         return acc + 1;
@@ -30,9 +30,20 @@ function notContainingEnoughNumbersAndNotLongEnough() {
     return "Password must be at least 8 characters\\nThe password must contain at least 2 numbers";
 }
 
+function hasCapitalLetter(password: string) {
+    const numberOfCapital: number = password.split("").reduce((acc, val) => {
+        if (!/[A-Z]/.test(val)) return acc;
+        return acc + 1;
+    },0);
+    return numberOfCapital > 0;
+}
+
 export function passwordValidation(password: string, errorMessage: string): IOutput {
     if (isLongEnough(password)) {
-        if (isContainEnoughNumbers(password)) return makeResponse(true, "");
+        if (hasEnoughNumbers(password)) {
+            if (!hasCapitalLetter(password)) return makeResponse(false, "password must contain at least one capital letter")
+            return makeResponse(true, "");
+        }
         return makeResponse(false, notContainingEnoughNumbersAndNotLongEnough());
     }
     return makeResponse(false, passwordNotLongEnough());
--- tests/passwordValidation.test.ts
+++ tests/passwordValidation.test.ts
@@ -18,7 +18,7 @@ describe('TU password validation', () => {
     describe('enough length', () => {
         test('it should allow a password long enough', async () => {
             // Arrange
-            const password = "abcde123";
+            const password = "Abcde123";
             const errorMessage = "";
             const isValid = true;
 
@@ -36,6 +36,22 @@ describe('TU password validation', () => {
             // Arrange
             const password = "somepassword";
             const errorMessage = "Password must be at least 8 characters\\nThe password must contain at least 2 numbers";
+            const isValid = false;
+
+            // Act
+            const output: IOutput = passwordValidation(password, errorMessage);
+
+            // Assert
+            expect(output.isValid).toEqual(isValid);
+            expect(output.errorMessage).toEqual(errorMessage);
+        });
+    });
+
+    describe('capital letter', () => {
+        test('it should warn me if password does not have capital letter', async () => {
+            // Arrange
+            const password = "somepassw123456";
+            const errorMessage = "password must contain at least one capital letter";
             const isValid = false;
 
             // Act
