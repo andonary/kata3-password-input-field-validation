cd6052251c341caa5ea1c54835905e2d23de1bcf
Ando Nary <ando.nary@gmail.com>
2021-12-19 12:40:55 +0100

    kata done

---
+++ src/passwordValidation.ts
@@ -0,0 +1,67 @@
+class Validator {
+  private errorMessages = "";
+
+  private addLine() {
+    if (this.errorMessages.length) this.errorMessages += "\\n";
+  }
+
+  notEnoughCharacters() {
+    this.errorMessages += "Password must be at least 8 characters";
+  }
+
+  notEnoughNumbers() {
+    this.addLine();
+    this.errorMessages += "The password must contain at least 2 numbers";
+  }
+
+  notEnoughCapitals() {
+    this.addLine();
+    this.errorMessages += "password must contain at least one capital letter";
+  }
+
+  notEnoughSpecials() {
+    this.addLine();
+    this.errorMessages += "password must contain at least one special character";
+  }
+
+  send(): IResult {
+    return {
+      isValid: this.errorMessages.length === 0,
+      errorMessages: this.errorMessages,
+    };
+  }
+}
+
+export class Password {
+  constructor(private password: string) {}
+
+  hasEnoughCharacters() {
+    return this.password.length > 7;
+  }
+
+  hasEnoughNumbers() {
+    return /.*\d+.*\d+.*/.test(this.password);
+  }
+
+  hasEnoughCapitals() {
+    return /.*[A-Z]+.*/.test(this.password);
+  }
+
+  hasEnoughSpecials() {
+    return /.*\W|_.*/.test(this.password);
+  }
+}
+
+export function passwordValidation(password: Password): IResult {
+  const validator: Validator = new Validator();
+  if (!password.hasEnoughCharacters()) validator.notEnoughCharacters();
+  if (!password.hasEnoughNumbers()) validator.notEnoughNumbers();
+  if (!password.hasEnoughCapitals()) validator.notEnoughCapitals();
+  if (!password.hasEnoughSpecials()) validator.notEnoughSpecials();
+  return validator.send();
+}
+
+export interface IResult {
+  isValid: boolean;
+  errorMessages: string;
+}
---
+++ tests/passwordValidation.test.ts
@@ -0,0 +1,121 @@
+import { IResult, Password, passwordValidation } from "../src/passwordValidation";
+
+describe("Password Validation", () => {
+  test.each([
+    {
+      condition: "if enough character",
+      password: "@B3p5oA",
+      isValid: false,
+      errorMessages: "Password must be at least 8 characters",
+    },
+    {
+      condition: "if enough character",
+      password: "@B3p5oA8",
+      isValid: true,
+      errorMessages: "",
+    },
+    {
+      condition: "if enough character",
+      password: "@B3p5oA89",
+      isValid: true,
+      errorMessages: "",
+    },
+    {
+      condition: "if enough numbers",
+      password: "@BEpSoAB1",
+      isValid: false,
+      errorMessages: "The password must contain at least 2 numbers",
+    },
+    {
+      condition: "if enough numbers",
+      password: "@BEpSoAB2",
+      isValid: false,
+      errorMessages: "The password must contain at least 2 numbers",
+    },
+    {
+      condition: "if enough numbers",
+      password: "@BEpSo5BR",
+      isValid: false,
+      errorMessages: "The password must contain at least 2 numbers",
+    },
+    {
+      condition: "if enough numbers",
+      password: "@BEpRoABR",
+      isValid: false,
+      errorMessages: "The password must contain at least 2 numbers",
+    },
+    {
+      condition: "if multiple validation errors",
+      password: "@BEpSoA",
+      isValid: false,
+      errorMessages: "Password must be at least 8 characters\\nThe password must contain at least 2 numbers",
+    },
+    {
+      condition: "if enough capital letter",
+      password: "@b3p5oa89",
+      isValid: false,
+      errorMessages: "password must contain at least one capital letter",
+    },
+    {
+      condition: "if enough capital letter",
+      password: "@z3p5oa89",
+      isValid: false,
+      errorMessages: "password must contain at least one capital letter",
+    },
+    {
+      condition: "if enough special character",
+      password: "AB3p5oa89",
+      isValid: false,
+      errorMessages: "password must contain at least one special character",
+    },
+    {
+      condition: "if enough special character",
+      password: "GB3p5oa89",
+      isValid: false,
+      errorMessages: "password must contain at least one special character",
+    },
+    {
+      condition: "if enough special character",
+      password: "G!3p5oa89",
+      isValid: true,
+      errorMessages: "",
+    },
+    {
+      condition: "if enough special character",
+      password: "G$3p5oa89",
+      isValid: true,
+      errorMessages: "",
+    },
+    {
+      condition: "if enough special character",
+      password: "G_3p5oa89",
+      isValid: true,
+      errorMessages: "",
+    },
+    {
+      condition: "if multiple validation errors",
+      password: "@bepsoa",
+      isValid: false,
+      errorMessages:
+        "Password must be at least 8 characters\\nThe password must contain at least 2 numbers\\npassword must contain at least one capital letter",
+    },
+    {
+      condition: "if multiple validation errors",
+      password: "abepsoa",
+      isValid: false,
+      errorMessages:
+        "Password must be at least 8 characters\\nThe password must contain at least 2 numbers\\npassword must contain at least one capital letter\\npassword must contain at least one special character",
+    },
+  ])("it should verify $condition: $password -> $isValid : $errorMessages", (cases) => {
+    // Arrange
+    const { password, isValid, errorMessages } = cases;
+    let passwordModel: Password = new Password(password);
+
+    // Act
+    const result: IResult = passwordValidation(passwordModel);
+
+    // Assert
+    expect(result.isValid).toEqual(isValid);
+    expect(result.errorMessages).toEqual(errorMessages);
+  });
+});
