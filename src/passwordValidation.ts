8e860213c5836b352cc550bf71daf7d11a682800
Ando Nary <ando.nary@gmail.com>
2022-01-25 09:15:17 +0100

    25 min

---
+++ src/passwordValidation.ts
@@ -0,0 +1,17 @@
+function invalidPassword(validation: any, message: string) {
+  validation.isValid = false;
+  if (validation.message) validation.message += "\n";
+  validation.message += message;
+}
+
+export function passwordValidation(input: string) {
+  let validation = {
+    isValid: true,
+    message: "",
+  };
+  if (input.length < 8) invalidPassword(validation, "Password must be at least 8 characters");
+  if (input.replaceAll(/\D/g, "").length < 2) invalidPassword(validation, "The password must contain at least 2 numbers");
+  if (input.replaceAll(/[^A-Z]/g, "") === "") invalidPassword(validation, "password must contain at least one capital letter");
+  if (input.replaceAll(/[\w]/g, "") === "") invalidPassword(validation, "password must contain at least one special character");
+  return validation;
+}
---
+++ tests/passwordValidation.test.ts
@@ -0,0 +1,34 @@
+import { passwordValidation } from "../src/passwordValidation";
+
+describe("Password validation", () => {
+  it("should valid password", async () => {
+    expect(passwordValidation("1De4çs7H")).toEqual({
+      isValid: true,
+      message: "",
+    });
+    expect(passwordValidation("1De4çs7")).toEqual({
+      isValid: false,
+      message: "Password must be at least 8 characters",
+    });
+    expect(passwordValidation("UDeQçsSH")).toEqual({
+      isValid: false,
+      message: "The password must contain at least 2 numbers",
+    });
+    expect(passwordValidation("1DeQçsSH")).toEqual({
+      isValid: false,
+      message: "The password must contain at least 2 numbers",
+    });
+    expect(passwordValidation("1DeQçsS")).toEqual({
+      isValid: false,
+      message: "Password must be at least 8 characters\nThe password must contain at least 2 numbers",
+    });
+    expect(passwordValidation("ude4çs7h")).toEqual({
+      isValid: false,
+      message: "password must contain at least one capital letter",
+    });
+    expect(passwordValidation("1De4cs7H")).toEqual({
+      isValid: false,
+      message: "password must contain at least one special character",
+    });
+  });
+});
