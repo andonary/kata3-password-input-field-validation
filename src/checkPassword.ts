ec2d79c9386b1676d809381fac1e7966564bb651
Ando Nary <ando.nary@gmail.com>
2022-03-31 21:51:57 +0200

    vite fait

--- src/checkPassword.ts
+++ src/checkPassword.ts
@@ -1,1 +1,31 @@
-export function checkPassword(input: string) {}
+function errorMessage(errorMsg: string, msg: string) {
+    if (msg.length) msg += '\n'
+    msg += errorMsg
+    return msg
+}
+
+export function checkPassword(input: string) {
+    let msg = ''
+    let isValid: boolean
+
+    if (input.length < 8)
+        msg = errorMessage('Password must be at least 8 characters', msg)
+    if (input.replaceAll(/\D/g, '').length < 2)
+        msg = errorMessage('Password must contain at least 2 numbers', msg)
+    if (input.replaceAll(/[^A-Z]/g, '').length < 1)
+        msg = errorMessage(
+            'Password must contain at least one capital' + ' letter',
+            msg
+        )
+    if (input.replaceAll(/\w/g, '').length < 1)
+        msg = errorMessage(
+            'Password must contain at least one special character',
+            msg
+        )
+
+    isValid = msg.length === 0
+    return {
+        isValid,
+        msg,
+    }
+}
--- tests/passwordValidation.test.ts
+++ tests/passwordValidation.test.ts
@@ -1,7 +1,43 @@
-import { checkPassword } from "../src/checkPassword";
+import { checkPassword } from '../src/checkPassword'
 
-describe("Password Validation", () => {
-  test("it should throw an error", async () => {
-    expect(() => checkPassword("")).toThrow("Password must be at least 8 characters");
-  });
-});
+describe('Password Validation', () => {
+    test('it should verify my password', () => {
+        expect(checkPassword('A2t4$s7')).toEqual({
+            isValid: false,
+            msg: 'Password must be at least 8 characters',
+        })
+
+        expect(checkPassword('A2t4$s78')).toEqual({
+            isValid: true,
+            msg: '',
+        })
+
+        expect(checkPassword('Adtq$sSH')).toEqual({
+            isValid: false,
+            msg: 'Password must contain at least 2 numbers',
+        })
+        expect(checkPassword('A2tq$sSH')).toEqual({
+            isValid: false,
+            msg: 'Password must contain at least 2 numbers',
+        })
+        expect(checkPassword('A2tq$s7H')).toEqual({
+            isValid: true,
+            msg: '',
+        })
+
+        expect(checkPassword('A2tq$sS')).toEqual({
+            isValid: false,
+            msg: 'Password must be at least 8 characters\nPassword must contain at least 2 numbers',
+        })
+
+        expect(checkPassword('a2tq$s7h')).toEqual({
+            isValid: false,
+            msg: 'Password must contain at least one capital letter',
+        })
+
+        expect(checkPassword('A2tqSs7h')).toEqual({
+            isValid: false,
+            msg: 'Password must contain at least one special character',
+        })
+    })
+})
